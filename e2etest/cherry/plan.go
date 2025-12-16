6b2f454a9fd4fb2419d8e244929c0885207714ef
Martynas Deveikis <martynasdev@gmail.com>
2025-12-16 17:00:08 +0200

    e2e-test: export plan constraint type

--- e2etest/cherry/plan.go
+++ e2etest/cherry/plan.go
@@ -19,13 +19,13 @@ func newPlanClient(c planClient) PlanClient {
 }
 
 // Pseudo-constant for the plan fields we want to get from the API.
-var planGetFields = []string{"slug", "type", "pricing", "available_regions"}
+var planGetFields = []string{"slug", "type", "pricing", "available_regions", "stock_qty"}
 
 // GetCheapest returns the slug of the cheapest plan for the billing cycle.
 func (c PlanClient) GetCheapest(
 	teamID int,
 	cycle string,
-	con ...planConstraint) (slug string, err error) {
+	con ...PlanConstraint) (slug string, err error) {
 	plans, _, err := c.c.List(teamID, &cherrygo.GetOptions{Fields: planGetFields})
 	if err != nil {
 		return "", fmt.Errorf("couldn't list plans: %w", err)
@@ -38,7 +38,7 @@ func (c PlanClient) GetCheapest(
 func getCheapestPlan(
 	plans []cherrygo.Plan,
 	cycle string,
-	con ...planConstraint) (cherrygo.Plan, error) {
+	con ...PlanConstraint) (cherrygo.Plan, error) {
 	var (
 		minIdx   = -1
 		minPrice float32
@@ -84,23 +84,23 @@ func getPlanPrice(p cherrygo.Plan, cycle string) (float32, error) {
 	return 0, fmt.Errorf("plan doesn't have a price for %q billing cycle", cycle)
 }
 
-type planConstraint func(cherrygo.Plan) bool
+type PlanConstraint func(cherrygo.Plan) bool
 
 // PlanTypeConstraint constrains plans by "type" field.
-func PlanTypeConstraint(t string) planConstraint {
+func PlanTypeConstraint(t string) PlanConstraint {
 	return func(p cherrygo.Plan) bool {
 		return p.Type == t
 	}
 }
 
 // PlanStockConstraint constrains plans to have enough stock.
-func PlanStockConstraint(region string, minStock int) planConstraint {
+func PlanStockConstraint(region string, minStock int) PlanConstraint {
 	return func(p cherrygo.Plan) bool {
 		return getPlanStock(p, region) >= minStock
 	}
 }
 
-func planFitsConstraints(p cherrygo.Plan, con ...planConstraint) bool {
+func planFitsConstraints(p cherrygo.Plan, con ...PlanConstraint) bool {
 	for _, c := range con {
 		if !c(p) {
 			return false
